d9174d5c610991908f29708db0d3159e8f99f2fa
xdavidwu <xdavidwuph@gmail.com>
2019-05-07 23:59:39 +0800

    mcc_download_new_lang: update to 1.14

--- mcc_download_new_lang.sh
+++ mcc_download_new_lang.sh
@@ -9,9 +9,9 @@ fi
 LANG_SUF=$(echo $1 | cut -d '_' -f 2 | tr -s '[:lower:]' '[:upper:]')
 
 #wget https://s3.amazonaws.com/Minecraft.Download/indexes/1.13.json
-wget https://launchermeta.mojang.com/v1/packages/68182cfc27e6807e7230740abb9e5ea56db7d0bf/1.13.1.json
+wget https://launchermeta.mojang.com/v1/packages/280eebe96a3ca45fcbc85800552cea775bc5f73c/1.14.json
 
-HASH=$(cat 1.13.1.json | tr '{' '\n' | grep minecraft -C 1 | grep "$1" -C 1 | tail -n 1 | cut -d '"' -f 4)
+HASH=$(cat 1.14.json | tr '{' '\n' | grep minecraft -C 1 | grep "$1" -C 1 | tail -n 1 | cut -d '"' -f 4)
 
 wget http://resources.download.minecraft.net/$(echo $HASH | head -c 2)/$HASH
 
