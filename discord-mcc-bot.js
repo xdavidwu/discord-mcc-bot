056c34605cf244d6a0f2b997a8936d47d3e161bb
xdavidwu <xdavidwuph@gmail.com>
2020-02-19 23:29:23 +0800

    update discord.js, use file for ip and token

--- discord-mcc-bot.js
+++ discord-mcc-bot.js
@@ -1,46 +1,48 @@
-const Discord=require('discord.js');
+const fs = require('fs');
+const Discord = require('discord.js');
 const { spawn } = require('child_process');
-const client=new Discord.Client();
-const mcc=spawn('mono',['../MinecraftClient.exe','test','-','1.2.3.4','BasicIO']);
+const client = new Discord.Client();
+const mcc = spawn('mono', ['MinecraftClient.exe', 'mcc', '-' ,
+		fs.readFileSync('ip', 'utf8').trim(), 'BasicIO']);
 
-ready=false;
-block=false;
+ready = false;
+block = false;
 
-client.on('ready',()=>{
+client.on('ready', () => {
 	console.log('ready');
-	ready=true;
+	ready = true;
 });
 
 mcc.stdout.on('data', (data) => {
-	str=data.toString('utf-8').replace(/§./g,'');
-	console.log('mcc: '+str);
-	if(str.match('^<.*> msgon\n$')){
-		block=false;
+	str = data.toString('utf-8').replace(/§./g, '');
+	console.log('mcc: ' + str);
+	if (str.match('^<.*> msgon\n$')) {
+		block = false;
 		mcc.stdin.write('bot: msg on\n');
 		return;
 	}
-	if(str.match('^<.*> msgoff\n$')){
-		block=true;
+	if (str.match('^<.*> msgoff\n$')) {
+		block = true;
 		mcc.stdin.write('bot: msg off\n');
 		return;
 	}
-	if(!ready){
+	if (!ready) {
 		console.log('not ready yet');
 		return;
 	}
-	const channel=client.channels.find(ch=>ch.name==='mcc');
-	if(!channel){
+	const channel = client.channels.cache.find(ch => ch.name === 'mcc');
+	if (!channel) {
 		console.log('#mcc not found');
 		return;
 	}
-	if(!block)channel.send(str);
+	if (!block) channel.send(str);
 });
 
 client.on('message', message => {
 	if (message.content.startsWith('.mcc ')) {
-		console.log('receive: '+message.content);
-		mcc.stdin.write(message.content.substring(5)+'\n');
+		console.log('receive: ' + message.content);
+		mcc.stdin.write(message.content.substring(5) + '\n');
 	}
 });
 
-client.login();
+client.login(fs.readFileSync('token', 'utf8').trim()).catch(console.err);
