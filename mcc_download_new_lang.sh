552fa29202e752c6d0de723a4e16dcc1f232fb2d
xdavidwu <xdavidwuph@gmail.com>
2020-02-19 23:26:12 +0800

    mcc_download_new_lang.sh: use jq, update to 1.15

--- mcc_download_new_lang.sh
+++ mcc_download_new_lang.sh
@@ -8,11 +8,11 @@ fi
 
 LANG_SUF=$(echo $1 | cut -d '_' -f 2 | tr -s '[:lower:]' '[:upper:]')
 
-#wget https://s3.amazonaws.com/Minecraft.Download/indexes/1.13.json
-wget https://launchermeta.mojang.com/v1/packages/280eebe96a3ca45fcbc85800552cea775bc5f73c/1.14.json
+#https://s3.amazonaws.com/Minecraft.Download/indexes/1.13.json
+#https://launchermeta.mojang.com/v1/packages/280eebe96a3ca45fcbc85800552cea775bc5f73c/1.14.json
 
-HASH=$(cat 1.14.json | tr '{' '\n' | grep minecraft -C 1 | grep "$1" -C 1 | tail -n 1 | cut -d '"' -f 4)
+HASH=$(wget https://launchermeta.mojang.com/v1/packages/28c11387f2c576240a31d359991bbaed28db57ab/1.15.json -O - | jq -r ".objects.\"minecraft/lang/$1.json\".hash")
 
-wget http://resources.download.minecraft.net/$(echo $HASH | head -c 2)/$HASH
+mkdir -p lang
 
-cat $HASH | tail -n +2 | head -n -1 | cut -d '"' -f 2-4 | sed 's/": "/=/' > lang/$(echo $1 | cut -d '_' -f 1)_$LANG_SUF.lang
+wget http://resources.download.minecraft.net/$(echo $HASH | head -c 2)/$HASH -O - | jq -r 'to_entries|map("\(.key)=\(.value)")[]' > lang/$(echo $1 | cut -d '_' -f 1)_$LANG_SUF.lang
